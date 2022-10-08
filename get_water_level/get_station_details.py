fca010ddd545b35c53593e40b787e9dc60abff47
Dou Mok <doumok@Dous-MacBook-Pro.local>
2022-10-08 11:33:18 -0700

    Re-organized files

--- get_water_level/get_station_details.py
+++ get_water_level/get_station_details.py
@@ -5,7 +5,7 @@ from lxml import etree
 from io import StringIO
 
 
-def get_station_list(url, station_id):
+def get_station_details(url, station_id):
     try:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
@@ -30,7 +30,23 @@ def get_station_list(url, station_id):
         # Decode bytes to string
         content = res.content.decode("utf-8")
 
-        target_file = f'./water_station.html'
+        parser = etree.HTMLParser()
+        content_to_parse = StringIO(content)
+        tree = etree.parse(content_to_parse, parser=parser)
+
+        station_list = []
+        station_rows = tree.xpath("//tbody/tr")
+        for row in station_rows:
+            station_cells = row.xpath("td")
+            station_list.append(
+                {
+                    "id": station_cells[3].text,
+                    "name": station_cells[1].xpath("label")[0].text,
+                    "province": station_cells[2].text
+                }
+            )
+
+        target_file = f'./_ReferenceExamples/water_station.html'
         with open(target_file, 'w') as f:
             f.write(content)
 
@@ -48,12 +64,13 @@ if __name__ == '__main__':
     # Endpoint
     station_detail_url = "https://wateroffice.ec.gc.ca/report/real_time_e.html"
 
-    station_list_with_ids = get_station_list(
+    # Retrieve the latest water level & historical graph data
+    station_list_with_ids = get_station_details(
         station_detail_url, station_id)
 
-    target_file = f'./water_station_list.json'
-    with open(target_file, 'w') as f:
-        f.write(json.dumps(station_list_with_ids))
+    # target_file = f'./water_station_detail.json'
+    # with open(target_file, 'w') as f:
+    #     f.write(json.dumps(station_list_with_ids))
 
     todays_date = date.today().strftime("%Y-%m-%d")
     disclaimer_info = f'Extracted from the Environment and Climate Change Canada Real-time Hydrometric Data web site (https://wateroffice.ec.gc.ca/mainmenu/real_time_data_index_e.html) on {todays_date}'
